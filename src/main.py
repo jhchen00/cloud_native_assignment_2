579b370104535196627e0bd3db741d143ec075aa
jhchen00 <66011494+jhchen00@users.noreply.github.com>
2025-04-03 17:53:08 +0800

    speed up

--- src/main.py
+++ src/main.py
@@ -8,7 +8,8 @@ def multiply(a: int, b: int) -> int:
     return a * b
 
 def fibonacci(n: int) -> int:
-    """ using recursion """
-    if n <= 1:
-        return n
-    return fibonacci(n - 1) + fibonacci(n - 2)
+    """ using dynamic programming """
+    fib = [0, 1]
+    for i in range(2, n + 1):
+        fib.append(fib[i - 1] + fib[i - 2])
+    return fib[n]
